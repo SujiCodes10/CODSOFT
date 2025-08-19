34226b5a7725b8e9f15164afd87c65d4e04c22b3
SujiCodes10 <sujithasujitha1004@gmail.com>
2025-08-19 05:56:04 +0530

    Create script.js

---
+++ LEVEL1_TASK2_Landing-Page/script.js
@@ -0,0 +1,19 @@
+const menuBtn =
+document.getElementById("menu-btn");
+const navLinks =
+document.querySelector(".nav-links");
+
+menuBtn.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor =>{
+    anchor.addEventListener("click", function(e) {
+        e.preventDefault();
+
+document.querySelector(this.getAttribute("href")).scrollIntoView({
+    behavior:"smooth"
+});
+navLinks.classList.remove("active");
+    });
+});
